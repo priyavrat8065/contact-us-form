f3d6b084921109e8b9e688d25a35c711c0e1a381
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2025-02-17 22:53:32 +0530

    show success message and reset fields

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const fields = document.querySelectorAll("input, textarea");
 const btn = document.querySelector(".btn");
 
+
 btn.addEventListener("click", function () {
+  isFieldEmpty = false;
   for (const el of document.querySelectorAll(".error-msg")) {
     el.classList.add("hidden");
   }
@@ -12,6 +14,7 @@ btn.addEventListener("click", function () {
     // console.log(box.value);
     // if (box.value === "") console.log(box.getAttribute("id"));
     if (field.value === "") {
+      isFieldEmpty = true;
       // console.log(`${field.getAttribute("id")} is empty`);
       const selector = `#${field.getAttribute("id")} + .error-msg`;
       console.log(selector);
@@ -22,6 +25,7 @@ btn.addEventListener("click", function () {
           count += 1;
         }
       } else if (!field.checked && field.getAttribute("id") === "consent") {
+        isFieldEmpty = true;
         document
           .querySelector(".consent .error-msg")
           .classList.toggle("hidden");
@@ -30,6 +34,15 @@ btn.addEventListener("click", function () {
   }
   console.log(count);
   if (count === 2) {
+    isFieldEmpty = true;
     document.querySelector(".query .error-msg").classList.toggle("hidden");
+  }
+  if (!isFieldEmpty) {
+    document.querySelector('.success-msg').classList.remove('hidden');
+    setTimeout( () => document.querySelector('.success-msg').classList.add('hidden'), 3000);
+    for (const field of fields) {
+      field.value = '';
+      field.checked = false;
+    }
   }
 });
