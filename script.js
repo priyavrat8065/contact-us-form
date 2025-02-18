2432d20c8d0274564dba2254b3c50e2a8e3a2984
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2025-02-18 09:31:32 +0530

    add comments

--- script.js
+++ script.js
@@ -3,28 +3,26 @@ const btn = document.querySelector(".btn");
 
 
 btn.addEventListener("click", function () {
+  // Reset the isFieldEmpty value to false
   isFieldEmpty = false;
+  // Hide all the unhidden error messages resulted from the previous submit button click
   for (const el of document.querySelectorAll(".error-msg")) {
     el.classList.add("hidden");
   }
   let count = 0;
   for (const field of fields) {
-    // console.log(field);
-    // console.log(field.getAttribute("id"));
-    // console.log(box.value);
-    // if (box.value === "") console.log(box.getAttribute("id"));
+    // check if textfield is empty
     if (field.value === "") {
       isFieldEmpty = true;
-      // console.log(`${field.getAttribute("id")} is empty`);
-      const selector = `#${field.getAttribute("id")} + .error-msg`;
       console.log(selector);
-      console.log(field.value);
       document.querySelector(`${selector}`).classList.toggle("hidden");
     } else {
+      // check if radio button is checked or not
       if (field.getAttribute("name") === "query") {
         if (!field.checked) {
           count += 1;
         }
+        // check if consent button checkbox is left unchecked or not
       } else if (!field.checked && field.getAttribute("id") === "consent") {
         isFieldEmpty = true;
         document
@@ -33,14 +31,17 @@ btn.addEventListener("click", function () {
       }
     }
   }
-  console.log(count);
+  // if count is equal to 2 it means neither radio button is checked
   if (count === 2) {
     isFieldEmpty = true;
     document.querySelector(".query .error-msg").classList.toggle("hidden");
   }
   if (!isFieldEmpty) {
+    // display the success message
     document.querySelector('.success-msg').classList.remove('hidden');
+    // hide the success message after 3 secs
     setTimeout( () => document.querySelector('.success-msg').classList.add('hidden'), 3000);
+    // Reset the whole form
     document.querySelector('form').reset();
   }
 });
